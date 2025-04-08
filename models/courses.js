b374eb12783c114fbbb041c52dc376ecc4ebe9eb
Firagos <jemalfiragos@gmail.com>
2025-04-09 00:01:00 +0300

    i create course model

---
+++ models/courses.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please provide a course title"],
+  },
+  instructor: {
+    type: String,
+    required: [true, "Please provide the instructor name"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please provide a course description"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please provide a course price"],
+  },
+  category: {
+    type: String,
+    required: [true, "Please provide a course category"],
+  },
+  content: [
+    {
+      moduleTitle: String,
+      videoUrl: String,
+      duration: Number, // Duration in minutes
+    },
+  ],
+  image: {
+    type: String, // Course thumbnail image URL
+    required: [true, "please provide a thubnail"],
+  },
+  rating: {
+    type: Number,
+    min: [1, "Rating must be at least 1"],
+    max: [5, "Rating must not exceed 5"],
+  },
+  students: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  cartData: {
+    type: Object,
+    default: {},
+  },
+});
+
+const Course = mongoose.model("Course", courseSchema);
+module.exports = Course;
