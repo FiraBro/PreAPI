4150c12ad1d5302b79b8ed3c7cf4c3a613f3c788
Firagos <jemalfiragos@gmail.com>
2025-04-09 10:53:55 +0300

    i finally modified app.js

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Routes
 const courseRoute = require("./routes/courseRouter");
 const userRouter = require("./routes/userRouter");
-// const cartRouter = require("./routes/cartRoutes");
+const cartRouter = require("./routes/cartRouter");
 
 app.use("/api/v1/courses", courseRoute);
 app.use("/api/v1/users", userRouter);
-// app.use("/api/v1/cart", cartRouter);
+app.use("/api/v1/cart", cartRouter);
 
 // Create uploads directory if it doesn't exist
 const fs = require("fs");
